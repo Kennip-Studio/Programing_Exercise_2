a4180cf7744aa043dbb699cea976a161a2f0627b
kennip <aa01092308481@gmail.com>
2019-10-16 13:42:35 +0900

    191016_01
    1回目の授業

---
+++ 01/HelloWorld.java
@@ -0,0 +1,7 @@
+import java.lang.*; // java.lang.* ‚Í default ‚Å import ‚³‚ê‚é‚Ì‚ÅÈ—ª‰Â
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
